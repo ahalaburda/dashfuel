a897e33036d6aa18ba1914a8cf6b1ba78dac0c3e
Adrian Halaburda <adrian@10grounds.com>
2023-06-18 18:33:32 -0400

    Linter and bug fix of customTimeline

--- components/CustomTimeline.js
+++ components/CustomTimeline.js
@@ -1,8 +1,8 @@
-import generateFakeData from "@/services/generateFakeData";
-import moment from "moment";
-import { useEffect, useState } from "react";
-import Timeline from "react-calendar-timeline";
-import "react-calendar-timeline/lib/Timeline.css";
+import generateFakeData from "@/services/generateFakeData"
+import moment from "moment"
+import { useEffect, useState } from "react"
+import Timeline from "react-calendar-timeline"
+import "react-calendar-timeline/lib/Timeline.css"
 
 const keys = {
   groupIdKey: "id",
@@ -14,58 +14,58 @@ const keys = {
   itemGroupKey: "group",
   itemTimeStartKey: "start",
   itemTimeEndKey: "end",
-  groupLabelKey: "title",
-};
-Object.freeze(keys);
+  groupLabelKey: "title"
+}
+Object.freeze(keys)
 
 export default function CustomTimeline() {
-  const [defaultTimeStart, setDefaultTimeStart] = useState();
-  const [defaultTimeEnd, setDefaultTimeEnd] = useState();
-  const [groups, setGroups] = useState([]);
-  const [items, setItems] = useState([]);
+  const [isReady, setIsReady] = useState(false)
+  const [defaultTimeStart, setDefaultTimeStart] = useState()
+  const [defaultTimeEnd, setDefaultTimeEnd] = useState()
+  const [groups, setGroups] = useState([])
+  const [items, setItems] = useState([])
 
   useEffect(() => {
-    const respData = generateFakeData();
-    const defaultTimeStartInitial = moment().startOf("day").toDate();
-    const defaultTimeEndInitial = moment()
-      .startOf("day")
-      .add(1, "day")
-      .toDate();
+    const respData = generateFakeData()
+    const defaultTimeStartInitial = moment().startOf("day").toDate()
+    const defaultTimeEndInitial = moment().startOf("day").add(1, "day").toDate()
 
-    setGroups(respData.groups);
-    setItems(respData.items);
-    setDefaultTimeStart(defaultTimeStartInitial);
-    setDefaultTimeEnd(defaultTimeEndInitial);
-  }, []);
+    setGroups(respData.groups)
+    setItems(respData.items)
+    setDefaultTimeStart(defaultTimeStartInitial)
+    setDefaultTimeEnd(defaultTimeEndInitial)
+    setIsReady(true)
+  }, [])
 
   const handleItemMove = (itemId, dragTime, newGroupOrder) => {
-    const group = groups[newGroupOrder];
-    const updateItems = items.map((item) =>
+    const group = groups[newGroupOrder]
+    const updateItems = items.map(item =>
       item.id === itemId
         ? Object.assign({}, item, {
             start: dragTime,
             end: dragTime + (item.end - item.start),
-            group: group.id,
+            group: group.id
           })
         : item
-    );
-    setItems(updateItems);
-    console.log("Moved", itemId, dragTime, newGroupOrder);
-  };
+    )
+    setItems(updateItems)
+    console.log("Moved", itemId, dragTime, newGroupOrder)
+  }
 
   const handleItemResize = (itemId, time, edge) => {
-    const updateItems = items.map((item) =>
+    const updateItems = items.map(item =>
       item.id === itemId
         ? Object.assign({}, item, {
             start: edge === "left" ? time : item.start,
-            end: edge === "left" ? item.end : time,
+            end: edge === "left" ? item.end : time
           })
         : item
-    );
-    setItems(updateItems);
-    console.log("Resized", itemId, time, edge);
-  };
+    )
+    setItems(updateItems)
+    console.log("Resized", itemId, time, edge)
+  }
 
+  if (!isReady) return null
   return (
     <div data-testid="CustomTimeline">
       <Timeline
@@ -73,17 +73,17 @@ export default function CustomTimeline() {
         groups={groups}
         items={items}
         keys={keys}
+        fullUpdate
         itemTouchSendsClick={false}
         stackItems
         itemHeightRatio={0.75}
         canMove={true}
-        canResize="both"
+        canResize={"both"}
         defaultTimeStart={defaultTimeStart}
         defaultTimeEnd={defaultTimeEnd}
         onItemMove={handleItemMove}
         onItemResize={handleItemResize}
-        fullUpdate
       />
     </div>
-  );
+  )
 }
